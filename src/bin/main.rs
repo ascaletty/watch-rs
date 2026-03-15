0f14517b7dcb34aaa00b423bef5ee38cfdf756ca
Aaron Scaletty <ascaletty23@gmail.com>
2026-03-15 17:18:56 -0500

    updated libaries and such

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -5,33 +5,31 @@
     reason = "mem::forget is generally not safe to do with esp_hal types, especially those \
     holding buffers for the duration of a data transfer."
 )]
-//! The following wiring is assumed:
-//LCD DC GPIO4
-//LCD CS GPIO5
-//LCD SDL GPIO6
-//LCD SDA GPIO7
-//LCD RST GPIO8
-//TP SCL ESP32_SCL
-//TP SDA ESP32_SDA
-//TP RST GPIO13
-//TP INT GPIO14
-//LCD BL GPIO15
-//Buzz GPIO42
-//BAT ADC GPIO1
-
+use core::fmt::Alignment;
 use core::pin::Pin;
+
+use alloc::string::ToString;
+use eg_seven_segment::SevenSegmentStyleBuilder;
 
 use bt_hci::cmd::info;
 use bt_hci::controller::ExternalController;
 use defmt::info;
 use embassy_executor::Spawner;
+use embassy_net::driver::Driver;
 use embassy_time::{Duration, Timer};
+use embedded_graphics::mono_font::ascii::FONT_10X20;
+use embedded_graphics::mono_font::{ascii::FONT_6X10, MonoTextStyle};
+use embedded_graphics::pixelcolor::BinaryColor;
+use embedded_graphics::primitives::{PrimitiveStyleBuilder, Rectangle};
+use embedded_graphics::text::{Baseline, TextStyleBuilder};
 use esp_hal::gpio::OutputPin;
-use esp_hal::spi::master::Config;
+use esp_hal::i2c::master::{AnyI2c, Config, I2c};
+use esp_hal::ledc::timer;
+use esp_hal::spi::master::Config as SConfig;
 use esp_hal::timer::systimer::SystemTimer;
 use esp_hal::timer::timg::TimerGroup;
+use esp_hal::{clock, peripherals, rng, spi, Async, Blocking, DriverMode};
 use esp_hal::{clock::CpuClock, gpio::OutputConfig};
-use esp_hal::{peripherals, spi, Blocking, DriverMode};
 use esp_wifi::ble::controller::BleConnector;
 use mipidsi::options::Orientation;
 use mipidsi::Display;
@@ -49,6 +47,7 @@ use embedded_graphics::{
     pixelcolor::Rgb565,
     prelude::*,
     primitives::{Circle, Primitive, PrimitiveStyle, Triangle},
+    text::Text,
 };
 
 // Provides the parallel port and display interface builders
@@ -82,16 +81,18 @@ async fn main(spawner: Spawner) {
 
     info!("Embassy initialized!");
 
-    // let rng = esp_hal::rng::Rng::new(peripherals.RNG);
-    // let timer1 = TimerGroup::new(peripherals.TIMG0);
-    // let wifi_init =
-    //     esp_wifi::init(timer1.timer0, rng).expect("Failed to initialize WIFI/BLE controller");
-    // let (mut _wifi_controller, _interfaces) = esp_wifi::wifi::new(&wifi_init, peripherals.WIFI)
-    //     .expect("Failed to initialize WIFI controller");
+    let timer1 = TimerGroup::new(peripherals.TIMG0);
+    let wifi_init =
+        esp_wifi::init(timer1.timer0, rng).expect("Failed to initialize WIFI/BLE controller");
+    let (mut _wifi_controller, interfaces) = esp_wifi::wifi::new(&wifi_init, peripherals.WIFI)
+        .expect("Failed to initialize WIFI controller");
+    let sniffer = interfaces.sta.capabilities();
+    info!("{:?}", sniffer);
     // // find more examples https://github.com/embassy-rs/trouble/tree/main/examples/esp32
     // let transport = BleConnector::new(&wifi_init, peripherals.BT);
+    // let rng = esp_hal::rng::Rng::new(peripherals.RNG);
     // let _ble_controller = ExternalController::<_, 20>::new(transport);
-    let mut buzzer = Output::new(peripherals.GPIO42, Level::Low, OutputConfig::default());
+    // let mut buzzer = Output::new(peripherals.GPIO42, Level::Low, OutputConfig::default());
     //LCD DC GPIO4
     //LCD CS GPIO5
     //LCD SDL GPIO6
@@ -113,7 +114,7 @@ async fn main(spawner: Spawner) {
     lcd_backligt.set_high();
     let mut delay = Delay::new();
     rst.set_high();
-    let mut spi = Spi::new(peripherals.SPI2, Config::default())
+    let mut spi = Spi::new(peripherals.SPI2, SConfig::default())
         .unwrap()
         .with_mosi(peripherals.GPIO7)
         .with_sck(peripherals.GPIO6);
@@ -131,16 +132,79 @@ async fn main(spawner: Spawner) {
     // display.sleep(&mut delay).unwrap();
     display.clear(Rgb565::new(200, 200, 200)).unwrap();
     info!("cleared display");
-    spawner.spawn(wake()).ok();
 
+    let style = PrimitiveStyleBuilder::new()
+        .stroke_color(Rgb565::RED)
+        .stroke_width(3)
+        .fill_color(Rgb565::GREEN)
+        .build();
+
+    info!("Hello world!");
+    Rectangle::new(Point::new(30, 20), Size::new(10, 15))
+        .into_styled(style)
+        .draw(&mut display)
+        .unwrap();
+    let style = MonoTextStyle::new(&FONT_10X20, Rgb565::WHITE);
+    let clockd = I2c::new(peripherals.I2C0, Config::default())
+        .unwrap()
+        .with_sda(peripherals.GPIO11)
+        .with_scl(peripherals.GPIO10)
+        .into_async();
+    let mut clock = pcf85063a::PCF85063::new(clockd);
+    clock.perform_software_reset().await.unwrap();
+    clock.start_clock().await.unwrap();
+    let mut prev = [' '; 8]; // "HH:MM:SS"
     loop {
-        info!("Hello world!");
-        Timer::after(Duration::from_secs(1)).await;
+        let hour = clock.get_datetime().await.unwrap().to_string();
+
+        info!("hour: {}", &hour.as_str());
+
+        let dt = clock.get_datetime().await.unwrap();
+
+        let mut now = [' '; 8];
+
+        now[0] = char::from_digit((dt.hour() / 10) as u32, 10).unwrap();
+        now[1] = char::from_digit((dt.hour() % 10) as u32, 10).unwrap();
+        now[2] = ':';
+        now[3] = char::from_digit((dt.minute() / 10) as u32, 10).unwrap();
+        now[4] = char::from_digit((dt.minute() % 10) as u32, 10).unwrap();
+        now[5] = ':';
+        now[6] = char::from_digit((dt.second() / 10) as u32, 10).unwrap();
+        now[7] = char::from_digit((dt.second() % 10) as u32, 10).unwrap();
+        let char_width = 12;
+
+        for i in 0..8 {
+            if now[i] != prev[i] {
+                let x = 60 + i as i32 * char_width;
+
+                Rectangle::new(Point::new(x, 130), Size::new(12, 22))
+                    .into_styled(
+                        PrimitiveStyleBuilder::new()
+                            .fill_color(Rgb565::new(200, 200, 200))
+                            .build(),
+                    )
+                    .draw(&mut display)
+                    .unwrap();
+
+                let mut buf = [0u8; 1];
+                buf[0] = now[i] as u8;
+
+                Text::new(
+                    core::str::from_utf8(&buf).unwrap(),
+                    Point::new(x, 150),
+                    style,
+                )
+                .draw(&mut display)
+                .unwrap();
+
+                prev[i] = now[i];
+            }
+        }
+
+        Timer::after_secs(1).await;
     }
 
+    // Create a text at position (20, 30) and draw it using the previously defined style
+
     // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0-rc.0/examples/src/bin
-}
-#[embassy_executor::task]
-async fn wake() {
-    info!("waking watch");
 }
